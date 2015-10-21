d24a20d8df624139c1d50f8ee0c216efa35828da
marvin <marvkipi@gmail.com>
2015-10-20 20:27:25 -0700

    add the ability to create admin users through admin backend

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -14,8 +14,19 @@ RSpec.feature "Admins can create users" do
 	scenario "with valid credentials" do 
 		fill_in "Email", with: "newbie@example.com"
 		fill_in "Password", with: "password"
+		
 		click_button "Create User"
 		expect(page).to have_content "User has been created."
 	end
+
+	scenario "when the new user is an admin" do
+		fill_in "Email", with: "admin@example.com"
+		fill_in "Password", with: "password"
+		check 'Is an admin?'
+		click_button "Create User"
+		expect(page).to have_content "User has been created"
+		expect(page).to have_content "admin@example.com"
+	end
 end
+
 
