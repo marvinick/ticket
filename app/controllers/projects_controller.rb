af66d2a5b087b119a083e597cc1ede64c92464f7
marvin <marvkipi@gmail.com>
2015-10-21 18:20:04 -0700

    don't list projects that user doesn't have permission to see

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 	before_action :set_project, only: [:edit, :update, :show]
 
 	def index
-		@projects = Project.all
+		@projects = policy_scope(Project)
 	end
 
 	def show
